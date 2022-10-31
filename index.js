169f54633b2216c7f982beb647dbc5be1bdafea4
Adam Courington <adamcourington@gmail.com>
2022-10-31 10:17:22 -0500

    added  menu Class and updated html

--- index.js
+++ index.js
@@ -26,3 +26,47 @@ class Recipe {
     return `${this.name} has ${this.ingredients.length} ingredients.`;
   }
 }
+
+class Menu {
+  constructor() {
+    this.recipes = [];
+    this.selectedRecipe = null;
+  }
+
+  start() {
+    let selection = this.showMainMenuOptions();
+    while (selection != 0) {
+      switch (selection) {
+        case '1':
+          this.createRecipe();
+          break;
+        case '2':
+          this.viewRecipe();
+          break;
+        case '3':
+          this.deleteRecipe();
+          break;
+        case '4':
+          this.displayRecipes();
+          break;
+        default:
+          selection = 0;
+      }
+      selection = this.showMainMenuOptions();
+    }
+
+    alert('Goodbye');
+  }
+  showMainMenuOptions() {
+    return prompt(`
+            0) Exit
+            1) Create New Recipe
+            2) View Recipe
+            3) Delete Recipe
+            4) Display All Recipes
+        `);
+  }
+}
+
+let menu = new Menu();
+menu.start();
