6a77fc7bb4fbc7f4e24c0b1514fe9b0050bbfde1
Adam Courington <adamcourington@gmail.com>
2022-10-31 10:05:40 -0500

    added Recipe Class

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-class Ingredients {
+class Ingredient {
   constructor(name, price) {
     this.name = name;
     this.price = price;
   }
   describe() {
     return `${this.name} costs ${this.price} dollars.`;
+  }
+}
+
+class Recipe {
+  constructor(name) {
+    this.name = name;
+    this.ingredients = [];
+  }
+  addIngredient(ingredient) {
+    if (ingredient instanceof Ingredient) {
+      this.ingredients.push(ingredient);
+    } else {
+      throw new Error(
+        `You can only add an instance of Ingredient.  Argument in not an ingredient: ${ingredient}`
+      );
+    }
+  }
+  describe() {
+    return `${this.name} has ${this.ingredients.length} ingredients.`;
   }
 }
